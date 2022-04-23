8c02764413a911de1f4da11a9d462d1a4435b73a
ricardosoaresds <rihcardosoares@gmail.com>
2022-04-23 12:49:18 -0300

    Projeto Uri em Phyton

---
+++ BEE1008/main.py
@@ -0,0 +1,9 @@
+#BEE1008
+
+NUMBER = int(input())
+NUMERODEHORAS = int(input())
+VALORDAHORA = float(input())
+
+SALARY = (NUMERODEHORAS*VALORDAHORA)
+print("NUMBER = %i" %NUMBER)
+print("SALARY = U$ %.2f" %SALARY)
