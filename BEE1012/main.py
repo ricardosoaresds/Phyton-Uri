ef4283869374239c1f7462de1872f44c75aca55e
ricardosoaresds <rihcardosoares@gmail.com>
2022-04-23 13:01:14 -0300

    Uri numero 1012

---
+++ BEE1012/main.py
@@ -0,0 +1,10 @@
+A = float(input())
+B = float(input())
+C = float(input())
+
+TRIANGULO = (A*C)/2
+CIRCULO = (C*C) * 3.14159
+
+
+print("TRIANGULO: %.3f" %TRIANGULO)
+print("CIRCULO: %3.f" %CIRCULO)
